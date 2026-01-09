d2c12fd27a141b28e2b0cd1118b52b12cccb60c0
Ella <ellalala.mc@gmail.com>
2026-01-09 22:51:44 +0800

    feat: :tada: Initial commit

---
+++ reverie/android/app/src/main/kotlin/com/example/reverie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reverie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ reverie/lib/core/theme/app_palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppPalette {
+  static const Color backgroundColor = Color.fromRGBO(244, 245, 255, 1);
+  static const Color primaryColor = Color.fromRGBO(75, 168, 200, 1);
+  static const Color textColor = Color.fromRGBO(28, 19, 48, 1);
+  static const Color warningColor = Color.fromRGBO(255, 219, 141, 1);
+  static const Color errorColor = Color.fromRGBO(229, 106, 108, 1);
+  static const Color successColor = Color.fromRGBO(111, 215, 186, 1);
+  static const Color purpleColor = Color.fromRGBO(171, 142, 228, 1);
+}
