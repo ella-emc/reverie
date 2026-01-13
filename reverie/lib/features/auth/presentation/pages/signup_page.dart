fd9d0613b9c44f8dd2940faa34ba69db11edbc4f
Ella <ellalala.mc@gmail.com>
2026-01-13 19:04:20 +0800

    feat: :sparkles: Add toggle visibility of password

--- reverie/lib/features/auth/presentation/pages/signup_page.dart
+++ reverie/lib/features/auth/presentation/pages/signup_page.dart
@@ -12,65 +12,100 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final formKey = GlobalKey<FormState>();
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isObscure = true;
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsetsGeometry.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Sign in to your account', style: GoogleFonts.playfairDisplay(
-              fontSize: 24,
-              color: AppPalette.primaryColor,
-              fontWeight: FontWeight.bold
-            )),
-            const SizedBox(height: 40),
-
-            // Username form field
-            Text('Username', style: GoogleFonts.dmSans(
-              color: AppPalette.textColor,
-              fontSize: 12,
-            )),
-            SizedBox(height: 4),
-            AuthField(placeholder: 'Your username', suffixIcon: Icons.person),
-
-            SizedBox(height: 24),
-
-            // Password form field
-            Text('Password', style: GoogleFonts.dmSans(
-              color: AppPalette.textColor,
-              fontSize: 12,
-            )),
-            SizedBox(height: 4),
-            AuthField(placeholder: 'Your password', suffixIcon: Icons.visibility_off),
-
-            SizedBox(height: 40),
-
-            // Button
-            Button(buttonText: 'Sign in'),
-            SizedBox(height: 24),
-            RichText(
-              text: TextSpan(
-                text: 'Don\'t have an account yet?',
-                style: GoogleFonts.dmSans(
-                  color: AppPalette.textColor,
-                  fontSize: 12,
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Sign in to your account', style: GoogleFonts.playfairDisplay(
+                fontSize: 24,
+                color: AppPalette.primaryColor,
+                fontWeight: FontWeight.bold
+              )),
+              const SizedBox(height: 40),
+          
+              // Username form field
+              Text('Username', style: GoogleFonts.dmSans(
+                color: AppPalette.textColor,
+                fontSize: 12,
+              )),
+              SizedBox(height: 4),
+              AuthField(
+                placeholder: 'Your username',
+                suffixIcon: const Icon(Icons.person, color: AppPalette.purpleColor, size: 16),
+                controller: usernameController,
+              ),
+          
+              SizedBox(height: 24),
+          
+              // Password form field
+              Text('Password', style: GoogleFonts.dmSans(
+                color: AppPalette.textColor,
+                fontSize: 12,
+              )),
+              SizedBox(height: 4),
+              AuthField(
+                placeholder: 'Your password',
+                obscureText: isObscure,
+                controller: passwordController,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isObscure ? Icons.visibility_off : Icons.visibility,
+                    color: AppPalette.purpleColor,
+                    size: 16,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isObscure = !isObscure;
+                    });
+                  },
                 ),
-                children: [
-                  TextSpan(
-                    text: ' Create an account',
-                    style: GoogleFonts.dmSans(
-                      color: AppPalette.primaryColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold
+              ),
+          
+              SizedBox(height: 40),
+          
+              // Button
+              Button(buttonText: 'Sign in'),
+              SizedBox(height: 24),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account yet?',
+                  style: GoogleFonts.dmSans(
+                    color: AppPalette.textColor,
+                    fontSize: 12,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: ' Create an account',
+                      style: GoogleFonts.dmSans(
+                        color: AppPalette.primaryColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold
+                      )
                     )
-                  )
-                ]
+                  ]
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
--- reverie/lib/features/auth/presentation/widgets/auth_field.dart
+++ reverie/lib/features/auth/presentation/widgets/auth_field.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:reverie/core/theme/app_palette.dart';
 
+
 class AuthField extends StatelessWidget {
   final String placeholder;
-  final IconData? suffixIcon;
-  static var _isVisible = false;
+  final Widget? suffixIcon;
+  final TextEditingController controller;
+  final bool obscureText;
+
   const AuthField({
     super.key,
     required this.placeholder,
     this.suffixIcon,
+    required this.controller,
+    this.obscureText = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       style: GoogleFonts.dmSans(
         fontSize: 12,
         color: AppPalette.textColor
       ),
       decoration: InputDecoration(
         hintText: placeholder,
-        suffixIcon: suffixIcon != null
-          ? Icon(
-              suffixIcon,
-              color: AppPalette.purpleColor,
-              size: 16,
-            )
-          : null,
+        suffixIcon: suffixIcon,
       ),
+      obscureText: obscureText,
+      validator: (value) {
+        if(value!.isEmpty) {
+          return '$placeholder is missing';
+        }
+        return null;
+      },
     );
   }
 }
